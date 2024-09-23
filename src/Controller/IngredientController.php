791a0826e9dea1548058646ba53045e3752b8859
stevenmarchive <marchive.pro@tutamail.com>
2024-09-23 12:47:45 +0200

    sauvegarde

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'ingredient')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('page/ingredient.html.twig', [
+            'ingredients' => $repository->findAll()
+        ]);
+    }
+}
